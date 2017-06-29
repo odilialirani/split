e38dd1a4f238e844ab59c3d3516ded57495a22c8
Odilia Lirani <oclirani@gmail.com>
2017-06-28 22:50:40 -0400

    added keys

---
+++ database.js
@@ -0,0 +1,30 @@
+var db_key = '14zvHC8wa4w1lWgNISOiOWPmHEsKCiR5Ab9mQuTxlR-0';
+var RECEIVE_URL = 'https://spreadsheets.google.com/feeds/list/14zvHC8wa4w1lWgNISOiOWPmHEsKCiR5Ab9mQuTxlR-0/1/public/basic?alt=json';
+var SUBMIT_URL = 'https://script.google.com/macros/s/AKfycbwaYFBAsmmVJqUTKxrjTSHxNYb4TJNzEEPer4EXYlwmSbqA3nI/exec';
+
+$(document).ready(function() {
+	$.ajax({
+		url: RECEIVE_URL,
+		success: function(data) {
+			console.log(data)
+			readDataAndAppend(data);
+		}
+	});
+
+	$("#submit_form").submit(function(event) {
+		event.preventDefault();
+		var data = $(this).serialize();
+		console.log(data);
+		$.ajax({
+			url: SUBMIT_URL,
+			type: "POST",
+			data: data
+		});
+
+	})
+})
+
+function readDataAndAppend(data) {
+	var allData = [];
+	var cells = data.feed.entry;
+}
