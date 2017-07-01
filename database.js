c6b070f2fb96e4d83660853f5d3c753526247423
Odilia Lirani <odilialirani@Odilias-Mac-mini.local>
2017-06-30 22:54:07 -0400

    getting user names

--- database.js
+++ database.js
@@ -2,12 +2,26 @@ var db_key = '14zvHC8wa4w1lWgNISOiOWPmHEsKCiR5Ab9mQuTxlR-0';
 var RECEIVE_URL = 'https://spreadsheets.google.com/feeds/list/14zvHC8wa4w1lWgNISOiOWPmHEsKCiR5Ab9mQuTxlR-0/1/public/basic?alt=json';
 var SUBMIT_URL = 'https://script.google.com/macros/s/AKfycbwaYFBAsmmVJqUTKxrjTSHxNYb4TJNzEEPer4EXYlwmSbqA3nI/exec';
 
+var RECEIVE_URL_USR = 'https://spreadsheets.google.com/feeds/list/1msMHb18JcFMfVmmoiFRyyj3rE74Zglot1SxpD0r9C0I/1/public/basic?alt=json';
+
+var usr;
+
 $(document).ready(function() {
 	$.ajax({
 		url: RECEIVE_URL,
 		success: function(data) {
-			console.log(data)
+			//console.log(data)
 			readDataAndAppend(data);
+		}
+	})
+
+	$.ajax({
+		url: RECEIVE_URL_USR,
+		success: function(data) {
+			//console.log(data)
+			usr = readData(data);
+			appendUser(usr);
+			//console.log(usr);
 		}
 	})
 
@@ -38,7 +52,8 @@ $(document).ready(function() {
 	})
 })
 
-function readDataAndAppend(data) {
+
+function readData(data) {
 	var allData = [];
 	var cells = data.feed.entry;
 
@@ -53,8 +68,28 @@ function readDataAndAppend(data) {
 		}
 		allData.push(rowObj);
 	}
+	//console.log(allData);
+	return allData;
+}
 
-	console.log(allData);
+
+function readDataAndAppend(data) {
+	var allData = readData(data);
+	var cells = data.feed.entry;
+
+	/*for (var i = 0; i < cells.length; i++) {
+
+		var rowObj = {};
+		rowObj.timestamp = cells[i].title.$t;
+		var rowCols = cells[i].content.$t.split(',');
+		for (var j = 0; j < rowCols.length; j++) {
+			var keyval = rowCols[j].split(':');
+			rowObj[keyval[0].trim()] = keyval[1].trim();
+		}
+		allData.push(rowObj);
+	}*/
+
+	//console.log(allData);
 	
 	// Showing all data on display.html
 
@@ -66,5 +101,18 @@ function readDataAndAppend(data) {
 		
 		var addRow = "<tr> <td>" + name + "</td><td>" + amount + "</td><td>" + desc + "</td></tr>";
 		$("#show_section").append(addRow)
+	}
+}
+
+function appendUser(data) {
+	for (var i = 0; i < data.length; i++) {
+		var obj = data[i];
+		var usrname = obj.username;
+
+		var addPayer = "<option>" + usrname + "</option>";
+		$("#payer").append(addPayer);
+
+		var addPayee = '<label class="checkbox-inline"><input type="checkbox" value="">' + usrname + '</label>';
+		$("#payee").append(addPayee);
 	}
 }
