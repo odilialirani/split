5e13d8b5a845102575e52acc1f1c09f92b3fe000
Odilia Lirani <oclirani@gmail.com>
2017-06-30 20:14:14 -0400

    trying user

---
+++ user.js
@@ -0,0 +1,3 @@
+var key = '1gMhVN9H5TP5YW2UX_Hp3Tm7KaWGWKeFZoaTzMja5Mb0';
+var RECEIVE_URL = 'https://spreadsheets.google.com/feeds/list/1gMhVN9H5TP5YW2UX_Hp3Tm7KaWGWKeFZoaTzMja5Mb0/1/public/basic?alt=json';
+var SUBMIT_URL = '';
